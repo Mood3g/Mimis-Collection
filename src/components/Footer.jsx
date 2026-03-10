9dca14e75c64574320b107bc993208a9f6950397
Miracle Okwiri <miracleokwiri690@gmail.com>
2026-03-10 11:30:09 +0100

    Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer class="bg-dark text-white py-4 mt-5">
-        <div class="container text-center">
+      <footer className="bg-dark text-white py-4 mt-5">
+        <div className="container text-center">
             <p>&copy; 2025 Mimi's Collection. All rights reserved.</p>
         </div>
     </footer>
